225ec1f40744ac2d41e15de331300a01f7f2c700
damsara-ekanayake <damsaraudana180@gmail.com>
2024-07-07 17:02:57 +0530

    2nd commit

--- Program.cs
+++ Program.cs
@@ -1,36 +1,15 @@
 ﻿Console.WriteLine($"  **************************************\n   WELCOME TO THE NUMBER GUESSING GAME \n  **************************************");
 
 bool isCorrectNumber  = false;
+bool isPlaying = true;
 Random random = new Random();
 int playerGuess;
 int randomNumber = random.Next(1, 100);
 
-GameFunction();
-PlayAgain();
-
-void PlayAgain()
+while (isPlaying)
 {
-    Console.Write("\nPLAY AGAIN [Y/N] .......... ");
-    var input = Console.ReadLine();
-
-    if (input == "Y" || input == "y")
-    {
-        randomNumber = random.Next(1, 100);
-        GameFunction();
-        Console.ReadKey();
-    }
-    else if (input == "N" || input == "n")
-    {
-        isCorrectNumber = true;
-        Console.WriteLine("\n\tTHANK YOU <3\n   BY MALFUNCTION SOFTWARE");
-        Console.ReadKey();
-    }
-    else
-    {
-        Console.WriteLine("INVALID INPUT. PLEASE TRY AGAIN");
-        PlayAgain();
-    }
-
+    GameFunction();
+    PlayAgain();
 }
 
 void GameFunction()
@@ -38,7 +17,7 @@ void GameFunction()
     do
     {
         Console.WriteLine("\n -- Enter a number between 1 and 100 --");
-        Console.WriteLine(randomNumber);
+        //Console.WriteLine(randomNumber);
         Console.Write("\n     ENTER YOUR GUESS HERE >>>>> ");
         var input = Console.ReadLine();
 
@@ -49,7 +28,7 @@ void GameFunction()
         {
             if (playerGuess < 1 || playerGuess > 100)
             {
-                Console.WriteLine("INVALID NUMBER. PLEASE ENTER A VAILD NUMBER\n");
+                Console.WriteLine(" INVALID NUMBER. PLEASE ENTER A VAILD NUMBER\n");
             }
             else
             {
@@ -59,10 +38,13 @@ void GameFunction()
         }
         else
         {
-            Console.WriteLine("INVALID INPUT. PLEASE ENTER A VALID INPUT\n");
+            Console.WriteLine(" INVALID INPUT. PLEASE ENTER A VALID INPUT\n");
         }
     }
     while (!isCorrectNumber);
+    //Console.WriteLine("loop ended");
+    //Console.WriteLine($" isCorrectNumber : {isCorrectNumber}");
+    //Console.WriteLine($" isPlaying :  {isPlaying}");
 }
 
 void CheckPlayerGuess()
@@ -80,4 +62,29 @@ void CheckPlayerGuess()
         Console.WriteLine(" \n  Congratulations! Your guess is correct \n\n*****************************************");
         isCorrectNumber = true;
     }
+}
+
+void PlayAgain()
+{
+    Console.Write("\n\tPLAY AGAIN [Y/N] .... ");
+    var input = Console.ReadLine();
+
+    if (input == "Y" || input == "y")
+    {
+        isCorrectNumber = false;
+        randomNumber = random.Next(1, 100);
+        //GameFunction();
+    }
+    else if (input == "N" || input == "n")
+    {
+        isPlaying = false;
+        Console.WriteLine("\n\tTHANK YOU <3\n   BY MALFUNCTION SOFTWARE");
+        Console.ReadKey();
+    }
+    else
+    {
+        Console.WriteLine("     INVALID INPUT. PLEASE TRY AGAIN");
+        PlayAgain();
+    }
+
 }
